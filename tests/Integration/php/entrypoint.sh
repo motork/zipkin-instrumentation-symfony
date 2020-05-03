4ae69859ec2e1ed733d93409a11af8de32cd3ba4
dirk39 <andreaquin1990@gmail.com>
2020-05-03 11:37:49 +0200

    wip dockerization (2)

--- tests/Integration/php/entrypoint.sh
+++ tests/Integration/php/entrypoint.sh
@@ -12,11 +12,11 @@ cat composer.json.dist \
 echo "cat composer.json"
 cat composer.json
 
-rm composer.lock
+rm composer.lock || true
 
 echo "Installing web-server-bundle"
 # web-server-bundle:4.4 supports ^3.4, ^4.0 and ^5.0 (see https://github.com/symfony/web-server-bundle/blob/4.4/composer.json#L23)
-composer require symfony/web-server-bundle:"^${SYMFONY_VERSION}|^4.4" --dev
+php -d memory_limit=-1 /usr/bin/composer require symfony/web-server-bundle:"^${SYMFONY_VERSION}|^4.4" --dev
 
 # includes configuration files to run the middleware in the app
 mv ./config/services.yaml ./config/services.yaml.dist
